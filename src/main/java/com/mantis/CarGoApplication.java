b83eeada354d43e4cc2837e5105fe681fbf86e3b
ramazan <ramazancetin@mantis.com.tr>
2023-07-04 09:16:32 +0300

    exclude DataSourceAutoConfiguration

--- src/main/java/com/mantis/CarGoApplication.java
+++ src/main/java/com/mantis/CarGoApplication.java
@@ -2,8 +2,10 @@ package com.mantis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CarGoApplication {
 
 	public static void main(String[] args) {
