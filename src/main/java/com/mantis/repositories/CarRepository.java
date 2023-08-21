6dff82ea39a4ff99b476b89e3a5487651e471c32
abdurrahmanyildirim <abdurrahmanyildirim@staj.com.tr>
2023-08-21 10:04:52 +0300

    Garage crud added to backend

---
+++ src/main/java/com/mantis/repositories/CarRepository.java
@@ -0,0 +1,13 @@
+package com.mantis.repositories;
+
+import com.mantis.data.entity.Car;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CarRepository extends JpaRepository<Car,Integer> {
+    @Query(value = "select  * from tbl_car where garage_id=:garageId",nativeQuery = true)
+    Page<Car> getCarsByGarageId(@Param("garageId") Integer garageId, Pageable pageable);
+}
